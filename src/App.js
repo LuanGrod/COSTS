8d67e5bc837076fdcad9299670a8ef7562035f58
LuanGrod <luan14rodrigues17@gmail.com>
2022-08-28 10:57:42 -0300

    28/08

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+
+import Container from './components/layout/Container'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/Contact">Contato</Link>
+        <Link to="/Company">Sobre</Link>
+        <Link to="/NewProject">Projetos</Link>
+      </div>
+
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/contact" element={<Contact/>}></Route>
+          <Route path="/company" element={<Company/>}></Route>
+          <Route path="/newproject" element={<NewProject/>}></Route>
+        </Routes>
+      </Container>
+
+      <p>footer</p>
+    </Router>
   );
 }
 
